c0c10ab1f1e716b2a5eb7d8b44d36413ed58f80e
jalvesneto <joalveslima14@gmail.com>
2022-01-13 08:54:53 -0300

    Update front e README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+#Login PGE
+
+Esse projeto foi construído utilizando os framework React e Node.js, e o banco de dados relacional, MySQL.
+
+##Instalação e execução
+Para instalação, faz-se necessário, no diretório _client_ executar o comando _npm install_ e em seguida _npm start_ para pôr em execução. O mesmo se faz necessário no diretório server.
+
+Quanto ao banco de dados, no arquivo server/index.js são definidas as configurações de conexão de banco, no caso, link, usuário, senha e nome do banco. No meu caso, foi definido localmente, por isso faz-se necessário a configuração. 
+
+Quanto ao SQL, deve-se ser criado o banco e as tabelas, que com os seguintes códigos SQL, pode-se fazer facilmente: 
+```
+CREATE SCHEMA `db` ;
+
+CREATE TABLE `db2`.`users` (
+  `idusers` INT NOT NULL AUTO_INCREMENT,
+  `user` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `level` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusers`));
+
+```
+
+Quanto as valores da tabela, existe um formulário comentado nomeado "form-cadastro" no arquivo client/app.js, que quando descomentado, permite ao usuário cadastrar um usuário para verificar o login. Outra forma de se adicionar um usuário é por comando SQL, que pode ser o seguinte:
+
+```
+INSERT INTO users (user, password, name, level) VALUES (user, password, name, level)
+
+```
--- client/src/App.js
+++ client/src/App.js
@@ -51,15 +51,17 @@ function App() {
   });
 
   return (
+    <div>
+      
     <div className="App">
       <img src="logo-2.png" />
       <br />
       <br />
-      <Formik initialValues={{}} onSubmit={addUser} validationSchema={validationAddUser}>
+      {/* <Formik initialValues={{}} onSubmit={addUser} validationSchema={validationAddUser} name="form-cadastro">
         <Form className='login-form'>
           <div>
           <span id="lblUsuario">Usuário</span>  <br /> 
-          <Field className="forml-field" name="user" placeholder="Usuário" /></div>
+          <Field className="form-field" name="user" placeholder="Usuário" /></div>
           <ErrorMessage
               component="span"
               name="user"
@@ -67,7 +69,7 @@ function App() {
             />
           <div>
           <span id="lblUsuario">Senha</span> <br /> 
-          <Field className="forml-field" name="password" placeholder="Senha" type="password"/></div>
+          <Field className="form-field" name="password" placeholder="Senha" type="password"/></div>
           <ErrorMessage
               component="span"
               name="password"
@@ -75,7 +77,7 @@ function App() {
             />
           <div>
           <span id="lblUsuario">Nome</span>  <br /> 
-          <Field className="forml-field" name="name" placeholder="Nome" /></div>
+          <Field className="form-field" name="name" placeholder="Nome" /></div>
           <ErrorMessage
               component="span"
               name="name"
@@ -83,7 +85,7 @@ function App() {
             />
           <div>
           <span id="lblUsuario">Nivel</span>  <br /> 
-          <Field className="forml-field" name="level" placeholder="Nível" /></div>
+          <Field className="form-field" name="level" placeholder="Nível" /></div>
           <ErrorMessage
               component="span"
               name="level"
@@ -92,24 +94,23 @@ function App() {
         <br />
         <input type="submit" name="btncadastrar" value="Cadastrar" id="btncadastrar" />
         </Form>
-      </Formik>
+      </Formik> */}
 
       <Formik initialValues={{}}
-        onSubmit={login}>
+        onSubmit={login} validationSchema={validationLogin} name="form-login">
         <Form className='login-form'>
         <span id="lblUsuario">Usuário</span>  
         <div> 
-        <Field className="forml-field" name="user" placeholder="Usuario" />
+        <Field className="form-field" name="user" placeholder="Digite o Usuário" />
         </div>
         <ErrorMessage
               component="span"
               name="user"
               className="form-error"
             />
-        
-        <span id="Label2">Senha</span><br />
         <div>
-        <Field className="forml-field" name="password" placeholder="Senha" type="password"/>
+        <span id="Label2">Senha</span><br />
+        <Field className="form-field" name="password" placeholder="Digite a Senha" type="password" />
         </div>
         <ErrorMessage
               component="span"
@@ -124,6 +125,40 @@ function App() {
         </Form>
       </Formik>
     </div>
+    <div class="col-lg-6 bg-image">
+                {/* <img src="fachada-natal.png"/> */}
+                <div class="col-md-5 alert alert-info object" role="alert">
+                    <div  style={{float: 'right', marginRight: '-45px', marginBottom: '-95px'}}>
+                        <a id="linkWhatsApp" class="tooltip-demo" href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=5579991912162" target="_blank">
+                        <i class="fab fa-whatsapp fa-5x" aria-hidden="true" title="Atendimento SGP"></i>
+                        </a>
+                    </div>
+                    <div class="col-lg-11 text-left">
+                        <p >
+                            <b>Atendimento pelo WhatsApp</b><br />
+                            <span class="footerInfo" ><b>Segunda a Sexta, das 7h às 13h.</b></span>
+                        </p>
+                    </div>
+                    <div class="col-lg-12 text-left">
+                        <div class="col-lg-6 nopadding">
+                            <p style={{width: '50%'}, {height: 'auto'}, {display:  'inline'}} >
+                                <b>
+                                    <a id="HyperLink2" href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=5579991912162" target="_blank" >(79) 99191-2162 <br /> <span>Suporte</span></a>
+                                </b>
+                            </p>
+                        </div>
+                        <div class="col-lg-6 nopadding">
+                            <p >
+                                <b>
+                                    <a id="HyperLink3" href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=5579991912482" target="_blank" >(79) 99191-2482 <br /> <span >Desenvolvimento</span></a>
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+  </div>
+
   );
   
 }
